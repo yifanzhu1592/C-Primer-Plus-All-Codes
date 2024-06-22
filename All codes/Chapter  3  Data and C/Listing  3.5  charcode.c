c26d3bd9274f1132041a7a6aa844dc7d204b8261
IlgizGalin <ilgiz.galin@icloud.com>
2024-06-22 15:31:27 +0500

    Logic error correction

--- All codes/Chapter  3  Data and C/Listing  3.5  charcode.c
+++ All codes/Chapter  3  Data and C/Listing  3.5  charcode.c
@@ -5,8 +5,8 @@ int main(void)
 	char ch;
 	
 	printf("Please enter a character.\n");
-	scanf("%c", &ch);   /* user inputs character */
-	printf("The code for %c is %d.\n", ch, ch);
+	scanf("%d", &ch);   /* user inputs character */
+	printf("The code for %d is %c.\n", ch, ch);
 	
 	return 0;
 }
